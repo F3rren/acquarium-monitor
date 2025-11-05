794b4970c862cf8839d22af0d427a636199aa09c
Samuele Alessandro Di Silvestri <samueledisilvestri@gmail.com>
2025-11-05 15:29:40 +0100

    First commit

---
+++ src/main/java/com/acquarium/acquarium/AcquariumApplication.java
@@ -0,0 +1,13 @@
+package com.acquarium.acquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquariumApplication.class, args);
+	}
+
+}
